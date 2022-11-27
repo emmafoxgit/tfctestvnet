b666c1e0000d69f29847c5e3a0083a2bf131635d
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-11-27 18:45:35 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,17 +7,6 @@
          # The name of the Terraform Cloud workspace to store Terraform state files in.
          workspaces {
            name = "# The configuration for the `remote` backend.
-#     terraform {
-#       backend "remote" {
-#         # The name of your Terraform Cloud organization.
-#         organization = "example-organization"
-#
-#         # The name of the Terraform Cloud workspace to store Terraform state files in.
-#         workspaces {
-#           name = "example-workspace"
-#         }
-#       }
-#     }"
          }
        }
      }
