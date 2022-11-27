fbd7f926556cb909ce47e37a872dad7ca73b1906
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-11-27 18:57:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "=3.0.1"
+      version = "=3.0.2"
     }
   }
-}
-
-# The configuration for the `remote` backend.
-     terraform {
-       backend "remote" {
+  cloud {
          # The name of your Terraform Cloud organization.
          organization = "emmafoxorg"
 
@@ -23,37 +20,16 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
+# 3. Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
+# 4. Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.example.id
-  }
-
-  tags = {
-    environment = "Production"
-  }
 }
