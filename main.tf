1c100981b8fd5610bf16d2cf356dc5efae839fd0
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-11-27 18:42:24 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+# The configuration for the `remote` backend.
+     terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "emmafoxorg"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "# The configuration for the `remote` backend.
+#     terraform {
+#       backend "remote" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "example-organization"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "example-workspace"
+#         }
+#       }
+#     }"
+         }
+       }
+     }
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
