844e6fa1701cefdba6d7df1283e035415088f894
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-12-22 15:50:16 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,15 +13,15 @@
 
 
 # 3. Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "qqqexample" {
+  name     = "qqqqexample-resources"
   location = "West Europe"
 }
 
 # 4. Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_virtual_network" "qqqexample" {
+  name                = "qqqqexample-network"
+  resource_group_name = azurerm_resource_group.qqqexample.name
+  location            = azurerm_resource_group.qqqexample.location
   address_space       = ["10.0.0.0/16"]
 }
